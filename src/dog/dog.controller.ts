c4418c51753ae825ac86b38124dc2aac502fe536
srxrt <str9703@gmail.com>
2024-11-16 16:51:53 +0900

    fix: create post method

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { DogService } from './dog.service';
 import { Request, Response } from 'express';
 
@@ -16,5 +25,10 @@ export class DogController {
     console.log('id', id);
     console.log('query', query);
     return this.dogService.introduce();
+  }
+  @Post('greet/edit')
+  public modifyDetail(@Body() body: any): string {
+    console.log('body', body);
+    return this.dogService.modifyDetail();
   }
 }
